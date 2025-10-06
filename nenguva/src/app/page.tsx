ab15e549dac99990e57c994f2fe76cba3325b3f8
HudsonWayne <waynewaynebenhura@gmail.com>
2025-10-06 14:56:41 +0200

    initial commit

--- nenguva/src/app/page.tsx
+++ nenguva/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
 import HeroSection from "@/components/HeroSection";
+import Companies from "@/components/companies"
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection/>
+      <Companies/>
     </div>
   );
 }
