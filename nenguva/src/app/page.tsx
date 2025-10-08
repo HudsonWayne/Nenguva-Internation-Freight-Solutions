9779ba9d8ebcac24df37c67f555267d0f1dca071
HudsonWayne <waynewaynebenhura@gmail.com>
2025-10-08 15:10:20 +0200

    initial commit

--- nenguva/src/app/page.tsx
+++ nenguva/src/app/page.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image";
-import Navbar from "@/components/navbar";
 import HeroSection from "@/components/HeroSection";
-import Companies from "@/components/companies"
+import Companies from "@/components/companies";
 import WhyUseNguva from "@/components/WhyUseNguva";
 import PopularServices from "@/components/PopularServices";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <HeroSection/>
-      <Companies/>
-      <WhyUseNguva/>
+      <HeroSection />
+      <Companies />
+      <WhyUseNguva />
       <PopularServices />
-      <Footer />
     </div>
   );
 }
